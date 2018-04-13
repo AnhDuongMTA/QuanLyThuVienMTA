1c0d246c616b988d38f9d86c096dc0c69a1e31be
minh <phamminh06101997@gmail.com>
2018-04-13 11:08:07 +0700

    [MINH] frm  Đăng NHập

--- QuanLyThuVienMTA/DAL/KetNoi.cs
+++ QuanLyThuVienMTA/DAL/KetNoi.cs
@@ -13,7 +13,7 @@ namespace DAL
         private SqlConnection conn;
         public KetNoi()
         {
-            conn = new SqlConnection(@"Data Source=ManhCuong-PC;Initial Catalog=QuanLyThuVienMTA;Integrated Security=True");
+            conn = new SqlConnection(@"Data Source=MINH-PC;Initial Catalog=QuanLyThuVienMTA;Integrated Security=True");
         }
         public DataTable GetData(string strSql)
         {
