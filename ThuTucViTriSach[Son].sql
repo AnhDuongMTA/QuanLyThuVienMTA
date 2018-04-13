f293631d8f4a79dda23c1ac83fe0b3a247a7be04
HongSonMTA <36963575+HongSonMTA@users.noreply.github.com>
2018-04-13 22:34:28 +0700

    [Sơn] Thủ Tục

---
+++ ThuTucViTriSach[Son].sql
@@ -0,0 +1,48 @@
+CREATE PROC XemThongTinViTri 
+AS
+BEGIN
+	SELECT*FROM dbo.ViTriSach
+END
+GO
+CREATE PROC ThemViTri (@MaVT VARCHAR(10),@TenKe NVARCHAR(50),@ViTri NVARCHAR(50))
+AS
+BEGIN
+	INSERT INTO dbo.ViTriSach
+	        ( MaVT, TenKe,ViTri )
+	VALUES  (@MaVT,@TenKe,@ViTri)
+END
+GO
+CREATE PROC SuaViTri (@MaVT VARCHAR(10),@TenKe NVARCHAR(50),@ViTri NVARCHAR(50))
+AS
+BEGIN
+	UPDATE dbo.ViTriSach
+	SET TenKe =@TenKe,ViTri =@ViTri
+	WHERE MaVT = @MaVT
+END
+GO
+CREATE PROC XoaViTri (@Ma VARCHAR(10))
+AS
+BEGIN
+	DELETE dbo.ViTriSach WHERE MaVT = @Ma
+END
+GO
+
+DELETE dbo.ChiTietPhieuMuon
+DELETE dbo.ChiTietSach
+DELETE dbo.ViTriSach
+GO
+
+CREATE PROC TTViTri (@Ma varchar(10))
+AS
+BEGIN
+	SELECT ViTriSach.MaVT,TenKe,ViTri,SUM(SoLuong) AS SoLuong FROM dbo.ViTriSach,dbo.ChiTietSach
+	WHERE ChiTietSach.MaVT = ViTriSach.MaVT AND ViTriSach.MaVT = @Ma
+	GROUP BY ViTriSach.MaVT,TenKe,ViTri
+END
+GO 
+CREATE PROC TTSach(@Ma VARCHAR(10))
+AS
+BEGIN
+	SELECT MaSach,TenSach,TacGia,TheLoai,NXB,NgonNgu,LinhVuc,NamXB,TenKe,SoLuong FROM dbo.ChiTietSach, dbo.ViTriSach 
+	WHERE ChiTietSach.MaVT = @Ma
+END
